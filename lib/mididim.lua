58681d3b0c1ecb346e21aaa070181255d8fdbb7e
Zack Scholl <zack.scholl@gmail.com>
2021-07-23 16:33:54 -0700

    play

--- lib/mididim.lua
+++ lib/mididim.lua
@@ -7,6 +7,7 @@ function Mididim:debug(s)
 end
 
 function Mididim:new(o)
+  -- need to set m
   o=o or {}
   setmetatable(o,self)
   self.__index=self
@@ -22,6 +23,15 @@ function Mididim:reset()
     self.quantize=0
   end
   self.memory={}
+  self:reset_note_ons()
+end
+
+function Mididim:reset_note_ons()
+  for k,v in pairs(self.on) do
+    self.m:note_off(v.note,v.velocity,v.ch)
+    self.on[k]=nil
+  end
+  self.on={}
 end
 
 function Mididim:start()
@@ -57,11 +67,15 @@ end
 
 function Mididim:msg(d)
   if self.is_recording then
-    table.insert(self.memory,d)
+    if d.type=="note_on" or d.type=="note_off"
+      or d.type=="pitchbend" or d.type=="key_pressure"
+      or d.type=="channel_pressure" then
+      table.insert(self.memory,d)
+    end
   end
 end
 
-function Mididim:play(m,global_beat)
+function Mididim:play(global_beat)
   if not self.is_playing then
     do return end
   end
@@ -71,27 +85,28 @@ function Mididim:play(m,global_beat)
     for i,v in ipairs(self.memory) do
       self.memory[i].played=false
     end
+    -- reset any playing notes
+    self:reset_note_ons()
   end
   self.last_beat=beat
   for i,v in ipairs(self.memory) do
     if (not v.played) and math.abs(v.beat-beat)<=self.quantize then
       self.memory[i].played=true
       if v.type=="note_on" then
-        m:note_on(v.note,v.velocity,v.ch)
+        self.m:note_on(v.note,v.velocity,v.ch)
+        self.on[v.note]={note=v.note,velocity=v.velocity,ch=v.ch}
       elseif v.type=="note_off" then
-        m:note_off(v.note,v.velocity,v.ch)
+        self.m:note_off(v.note,v.velocity,v.ch)
+        self.off[v.note]=nil
       elseif v.type=="pitchbend" then
-        m:pitchbend(v.val,v.ch)
+        self.m:pitchbend(v.val,v.ch)
       elseif v.type=="key_pressure" then
-        m:key_pressure(v.note,v.val,v.ch)
+        self.m:key_pressure(v.note,v.val,v.ch)
       elseif v.type=="channel_pressure" then
-        m:channel_pressure(v.val,v.ch)
+        self.m:channel_pressure(v.val,v.ch)
       end
     end
   end
 end
 
--- return Mididim
-
-mode_debug=true
-m=Mididim:new()
+return Mididim
